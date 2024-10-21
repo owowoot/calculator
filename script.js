5c556d48b8979c41f78864732ac1b77016e1a618
Richard <richardzou25@gmail.com>
2024-10-21 12:38:25 -0400

    basic functions for each of the four operations: add, subtract, multiply, divide

---
+++ script.js
@@ -0,0 +1,22 @@
+// Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, 
+// so start by creating functions for the following items and testing them in your browser’s console.
+// add
+function addition(num1, num2) {
+    let result = num1 + num2;
+    return result;
+}
+// subtract
+function subtraction(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+// multiply
+function multiplication(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+// divide
+function division(num1, num2) {
+    let result = num1 / num2;
+    return result;
+}
