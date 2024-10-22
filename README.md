819e699fb5d0a32da50c09ed9391e4643619bf40
Richard <richardzou25@gmail.com>
2024-10-22 11:46:04 -0400

    Updates and additions - set up html boilerplate - changed operator functions to return result instead of creating a result variable first - storing calculating inputs variables created.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # calculator
 
 ```
-Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console.
-add
-subtract
-multiply
-divide
+## Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console.
+<ul>
+<li>add</li>
+<li>subtract</li>
+<li>multiply</li>
+<li>divide</li>
+</ul>
 A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
 Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 Create a basic HTML calculator with buttons for each digit, each of the above functions and an “Equals” key.
--- script.js
+++ script.js
@@ -2,21 +2,28 @@
 // so start by creating functions for the following items and testing them in your browser’s console.
 // add
 function addition(num1, num2) {
-    let result = num1 + num2;
-    return result;
+    return num1 + num2;
 }
 // subtract
 function subtraction(num1, num2) {
-    let result = num1 - num2;
-    return result;
+    return num1 - num2;
 }
 // multiply
 function multiplication(num1, num2) {
-    let result = num1 * num2;
-    return result;
+    return num1 * num2;
 }
 // divide
 function division(num1, num2) {
-    let result = num1 / num2;
-    return result;
+    if (num2 === 0) {
+        return "ERROR: Division by 0"
+    }
+    return num1 / num2;
 }
+// A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
+// Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, 
+// and the second number. You’ll use these variables to update your display later.
+let num1 = ''
+let num2 = ''
+let op = ''
+
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
