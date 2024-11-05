9f1412e3e2cd56d85d9344128217ded3e560bd4c
Richard <richardzou25@gmail.com>
2024-11-05 11:37:44 -0500

    simple gui to work with

--- script.js
+++ script.js
@@ -22,9 +22,10 @@ function division(num1, num2) {
 // A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
 // Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, 
 // and the second number. You’ll use these variables to update your display later.
-let num1 = ''
-let num2 = ''
-let op = ''
+let num1 = '';
+let num2 = '';
+let op = '';
+let result = '';
 
 // Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
 function operate(num1, num2, op) {
@@ -43,26 +44,42 @@ function operate(num1, num2, op) {
 }
 
 const display = document.querySelector('.display');
-// Update display on button call
 const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+// Update display on button call
 numberButtons.forEach(button => {
     button.addEventListener('click', function () {
+        if (display.textContent === '0' || op) {
+            display.textContent = '';
+            op = '';
+        }
         display.textContent += button.textContent;
     });
 });
 // Perform operations
-document.querySelector('.equals').addEventListener('click', function () {
-    num2 = parseFloat(display.textContent);
-    let result = operate(num1, num2, op);
-    display.textContent = result;
-    num1 = result
-});
+operatorButtons.forEach(button => {
+    button.addEventListener('click', function () {
+        num1 = display.textContent;
+        op = button.textContent;
+        display.textContent = '0';
+    })
+})
+// Handle Equals
+equalsButton.addEventListener('click', function () {
+    num2 = display.textContent;
+    result = operate(num1, num2, op);
+    display.textContent(Math.round(result * 100) / 100);
+    num1 = result;
+    op = ''
+})
+
 // Handle clear
-document.querySelector('clear').addEventListener('click', function () {
+clearButton.addEventListener('click', function () {
     num1 = '';
     num2 = '';
     op = '';
+    result = '';
     display.textContent = '0';
-})
-// Handle edge cases
-result = Math.round(result * 100) / 100;
+});
